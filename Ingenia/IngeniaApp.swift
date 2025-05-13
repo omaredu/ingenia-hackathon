afff7a53cfb3175835d4cc09a2e9ded18194f187
Omar Sánchez <me@omaredu.com>
2025-05-12 21:35:37 -0600

    Initial Commit

---
+++ Ingenia/IngeniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IngeniaApp.swift
+//  Ingenia
+//
+//  Created by Omar Sánchez on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct IngeniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
